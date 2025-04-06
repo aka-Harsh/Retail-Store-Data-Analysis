a5478ef29135778f9f4c7109ff7ca0ee760a51dd
Harsh Mehta <harsh.work8421@gmail.com>
2025-04-06 10:32:13 +0530

    Admin Dashboard is created in pages

---
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,8 @@
+// src/pages/admin/AdminDashboard.jsx
+import Dashboard from '../../components/admin/Dashboard';
+
+const AdminDashboard = () => {
+  return <Dashboard />;
+};
+
+export default AdminDashboard;
