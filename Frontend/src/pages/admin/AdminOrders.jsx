3f95afe16359a91cc73d9d90db6982ff8d7c041e
Harsh Mehta <harsh.work8421@gmail.com>
2025-04-06 10:33:00 +0530

    Admin Order is created in pages

---
+++ Frontend/src/pages/admin/AdminOrders.jsx
@@ -0,0 +1,8 @@
+// src/pages/admin/AdminOrders.jsx
+import OrderManagement from '../../components/admin/OrderManagement';
+
+const AdminOrders = () => {
+  return <OrderManagement />;
+};
+
+export default AdminOrders;
