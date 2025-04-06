3324cbcee74f59975f2625e075ceb57d64bfdbee
Harsh Mehta <harsh.work8421@gmail.com>
2025-04-06 10:33:19 +0530

    AdminReport is created in pages

---
+++ Frontend/src/pages/admin/AdminReports.jsx
@@ -0,0 +1,8 @@
+// src/pages/admin/AdminReports.jsx
+import SalesReport from '../../components/admin/SalesReport';
+
+const AdminReports = () => {
+  return <SalesReport />;
+};
+
+export default AdminReports;
