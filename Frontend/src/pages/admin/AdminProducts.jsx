ee2b04f6c5de0d5edf111415dcc87eae6f5513cd
Harsh Mehta <harsh.work8421@gmail.com>
2025-04-06 10:32:35 +0530

    AdminProduct is created in pages

---
+++ Frontend/src/pages/admin/AdminProducts.jsx
@@ -0,0 +1,8 @@
+// src/pages/admin/AdminProducts.jsx
+import ProductManagement from '../../components/admin/ProductManagement';
+
+const AdminProducts = () => {
+  return <ProductManagement />;
+};
+
+export default AdminProducts;
